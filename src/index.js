a6918f52a94e175b6dcd79a28145033d77f2d427
birendrajha <birendrajha1500123@gmail.com>
2020-12-15 15:29:58 +0530

    commit first

--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ const app = express()
 const port = 8080
 
 const onePageArticleCount = 10
-
+require('./createDatabase.js')
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+ app.get('/newFeeds',async(req,res)=>{
+          let limit =Number(req.query.limit);
+          let offset = Number(req.query.offset);
+          if(limit==0 || isNaN(limit)){
+                limit = 10;
+          }
+          if(offset==0 || isNaN(offset)){
+            offset = 0;
+      }
+      try{
+      const data = await newsArticleModel.find().limit(limit).skip(offset);
+      res.status(200).send(data);
+      }catch(err){
+               res.status(400).send(err);
+      } 
+ })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
